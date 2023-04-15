29be34d288c897783c935bdc7cb95729690df7ac
Lucas <lucasgoncalves08@live.com>
2023-04-15 18:12:11 -0300

    creating the counter

--- app/validatesTheGuess.js
+++ app/validatesTheGuess.js
@@ -1,6 +1,11 @@
+const counterSpan = document.querySelector('#counter');
+
+let counter = 0;
+
 function validatesTheGuess (guess, secretNumber) {
 
     const guessValid = +guess;
+    console.log(secretNumber)
    
     if (Number.isNaN(guessValid)) {
             
@@ -23,14 +28,20 @@ function validatesTheGuess (guess, secretNumber) {
 
     if(guessValid === secretNumber) {
 
+        counter++
+
         document.body.innerHTML = `
-            <h1 class="font-bold mb-20 text-4xl sm:text-6xl">Congratulations!</h1> 
+            <h1 class="font-bold mt-16 mb-20 text-4xl sm:text-6xl">Congratulations!</h1> 
 
             <h3 class="font-bold mx-12 mb-12 text-2xl w-64 sm:text-3xl w-128">The secret number was <span class="text-DarkBlue">${secretNumber}</span></h3> 
+
+            <h3 class="italic font-semibold text-LightGreen mx-12 mb-12 text-xl w-64 sm:text-2xl w-128">You got it in ${counter} attempts</h3> 
 
             <input type="button" class="bg-SpecialWhite rounded text-DarkBlue font-semibold cursor-pointer text-xl p-2 m-8 hover:bg-DarkBlue hover:text-SpecialWhite" value="Play again" onclick=window.location.reload()>`
 
     } else if (guessValid < secretNumber) {
+
+        counter++
 
         guessDiv.innerHTML = `
             <h3 class="text-SpecialWhite font-bold mb-8">You said:</h3> 
@@ -39,7 +50,11 @@ function validatesTheGuess (guess, secretNumber) {
 
             <h3 class="font-bold text-LightGreen mt-6 mx-4">Hint: the secret number is bigger <i class="fa-solid fa-arrow-up-long fa-bounce"></i></h3>`
 
+            
+
     } else {
+
+        counter++
 
         guessDiv.innerHTML = `
             <h3 class="text-SpecialWhite font-bold mb-8">You said:</h3> 
@@ -49,4 +64,8 @@ function validatesTheGuess (guess, secretNumber) {
             <h3 class="font-bold text-LightGreen mt-6 mx-4">Hint: the secret number is smaller <i class="fa-solid fa-arrow-down-long fa-bounce"></i></h3>`
 
     }
+
+    counterSpan.innerHTML = `${counter}`;
 };
+
+
